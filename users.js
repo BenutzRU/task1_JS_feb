351d9f11fa51cd4f5b4a7de9f4f014e980fe47ad
Ilya <107938524+BenutzRU@users.noreply.github.com>
2024-02-18 18:56:48 +0300

    users.js

--- users.js
+++ users.js
@@ -1,43 +1,42 @@
 async function methodGetTodos() {
-    let xhrTodos = new XMLHttpRequest();
-    xhrTodos.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-    xhrTodos.send();
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos', true);
+    xhr.send();
 
-    xhrTodos.onload = function () {
-      let todos = JSON.parse(xhrTodos.response);
-
-      if (Array.isArray(todos) && todos.length > 0) {
-       
-        let xhrUsers = new XMLHttpRequest();
-        xhrUsers.open('GET', 'https://jsonplaceholder.typicode.com/users');
-        xhrUsers.send();
-
-        xhrUsers.onload = function () {
-          let users = JSON.parse(xhrUsers.response);
-
-          if (Array.isArray(users) && users.length > 0) {
-            
-            let userMap = {};
-            users.forEach(user => {
-              userMap[user.id] = user.name;
-            });
-
-            
-            todos.forEach((todo, index) => {
-              let row = '<tr>';
-              row += '<td>' + (index + 1) + '</td>';
-              row += '<td>' + userMap[todo.userId] + '</td>';
-              row += '<td>' + todo.title + '</td>';
-              row += '<td><input type="checkbox" ' + (todo.completed ? 'checked' : '') + ' disabled/></td>';
-              row += '</tr>';
-
-              $('table tbody').append(row);
-            });
-          }
-        };
-      }
+    xhr.onload = async function () {
+        if (xhr.status == 200) {
+            let todos = JSON.parse(xhr.responseText);
+            await getUsersAndRenderTable(todos);
+        }
     };
-  }
-function clearTodoList() {
-    $('table tbody').empty(); 
+}
+
+async function getUsersAndRenderTable(todos) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    xhr.send();
+
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            let users = JSON.parse(xhr.responseText);
+            renderTable(todos, users);
+        }
+    };
+}
+
+function renderTable(todos, users) {
+    $('table tbody').empty();
+
+    todos.forEach((todo, index) => {
+        let user = users.find(user => user.id === todo.userId);
+
+        let row = '<tr>';
+        row += '<td>' + (index + 1) + '</td>';
+        row += '<td>' + (user ? user.name : '') + '</td>';
+        row += '<td>' + todo.title + '</td>';
+        row += '<td><input type="checkbox" ' + (todo.completed ? 'checked' : '') + ' disabled></td>';
+        row += '</tr>';
+
+        $('table tbody').append(row);
+    });
 }
